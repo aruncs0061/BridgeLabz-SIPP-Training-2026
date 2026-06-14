55e3d8020de5a0ed13b19af0560f6beaffffa5b1
aruncs0061 <anshuchaurasia19100@gmail.com>
2026-06-14 20:11:40 +0530

    Delete CoreJava/gcr-codebase directory

--- CoreJava/gcr-codebase/programming-elements/Level1/AgeCalculator.java
+++
@@ -1,8 +0,0 @@
-class AgeCalculator{
-    public static void main(String[] args){
-        int birthYear=2000;
-        int currentYear=2024;
-        int currentAge=currentYear-birthYear;
-        System.out.println("Harry's age is "+currentAge);
-    }
-}
--- CoreJava/gcr-codebase/programming-elements/Level1/KilometersToMiles.java
+++
@@ -1,8 +0,0 @@
-class KilometersToMiles {
-    public static void main(String[] args){
-        double distanceInKm=10.8;
-        double kmPerMile=1.6;
-        double distanceInMiles=distanceInKm/kmPerMile;
-        System.out.println("The distance "+distanceInKm+" km in miles is "+distanceInMiles);
-    }  
-}
